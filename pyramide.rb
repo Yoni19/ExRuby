584d0e1f19b60b74b7f18d20e9b0a3a8c8a10cc7
Yoni19 <anthony.krief@hotmail.fr>
2020-07-07 20:32:23 +0200

    Modif Antho ex

--- pyramide.rb
+++ pyramide.rb
@@ -1,5 +1,9 @@
 puts "Salut, bienvenue dans ma super pyramide ! Combien d'étages veux-tu ?"
 etage = Integer(gets)
+if (etage < 1) or (etage > 25)
+    puts " NOOOON"
+    exit
+end
 x = 1
 
 puts "Voici la pyramide : "
