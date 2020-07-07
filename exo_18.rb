db61c6ac235703b190bba4e8b0ec9ff2b714a949
Yoni19 <anthony.krief@hotmail.fr>
2020-07-07 17:33:02 +0200

    maj Anthony exo

--- exo_18.rb
+++ exo_18.rb
@@ -8,14 +8,15 @@ while i < 50 do
 if i < 10
     1.times do
         tab<< "jean.dupont.0#{i}@email.fr"
-        puts "jean.dupont.0#{i}@email.fr"
+    
     end
 else
 1.times do
     tab<< "jean.dupont.#{i}@email.fr"
-    puts "jean.dupont.#{i}@email.fr"
+    
     end
     
 end
 end
 
+puts tab.inspect
